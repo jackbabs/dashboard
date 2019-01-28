26610995a69cbbb97a442b8122c0e1a1e3bd7fe4
jackbaber <thejackbabs@gmail.com>
2019-01-28 14:57:23 +0000

    setup

---
+++ config/passport.js
@@ -0,0 +1,14 @@
+module.exports = passport => {
+  passport.use(
+    new JwtStrategy(opts, (jwt_payload, done) => {
+      User.findById(jwt_payload.id)
+        .then(user => {
+          if (user) {
+            return done(null, user);
+          }
+          return done(null, false);
+        })
+        .catch(err => console.log(err));
+    })
+  );
+};
