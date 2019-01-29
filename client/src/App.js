b934d917dd85f788c3c44665458b2c738ca848e7
jackbaber <thejackbabs@gmail.com>
2019-01-29 10:58:09 +0000

    changed submodule to directory

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">Test</div>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
